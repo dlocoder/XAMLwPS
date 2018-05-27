7135fd7e94e6914f95caaaea4c51ef79b30d916e
dlocoder <39681190+dlocoder@users.noreply.github.com>
2018-05-27 15:19:37 -0700

    Add files via upload

---
+++ UsrControl.ps1
@@ -0,0 +1,69 @@
+﻿Remove-Variable * -ErrorAction SilentlyContinue
+
+#Get Wizard script parent directory 
+If (Test-Path -LiteralPath 'variable:HostInvocation') { $InvocationInfo = $HostInvocation } Else { $InvocationInfo = $MyInvocation }
+[string]$scriptDirectory = Split-Path -Path $InvocationInfo.MyCommand.Definition -Parent
+
+
+function Get-XamlObject
+{
+	[CmdletBinding()]
+	param (
+		[Parameter(Position = 0,
+				   Mandatory = $true,
+				   ValuefromPipelineByPropertyName = $true,
+				   ValuefromPipeline = $true)]
+		[Alias("FullName")]
+		[System.String[]]$Path
+	)
+
+	BEGIN
+	{
+		Set-StrictMode -Version Latest
+
+		$wpfObjects = @{ }
+        Add-Type -AssemblyName PresentationFramework, PresentationCore, WindowsBase, System.Windows.Forms, System.Drawing
+		#Add-Type -AssemblyName presentationframework, presentationcore
+
+	} #BEGIN
+
+	PROCESS
+	{
+		try
+		{
+			foreach ($xamlFile in $Path)
+			{
+				#Change content of Xaml file to be a set of powershell GUI objects
+				$inputXML = Get-Content -Path $xamlFile -ErrorAction Stop
+				$inputXMLClean = $inputXML -replace 'mc:Ignorable="d"', '' -replace "x:N", 'N' -replace 'x:Class=".*?"', '' -replace 'd:DesignHeight="\d*?"', '' -replace 'd:DesignWidth="\d*?"', ''
+				[xml]$xaml = $inputXMLClean
+				$reader = New-Object System.Xml.XmlNodeReader $xaml -ErrorAction Stop
+				$tempform = [Windows.Markup.XamlReader]::Load($reader)
+
+				#Grab named objects from tree and put in a flat structure
+				$namedNodes = $xaml.SelectNodes("//*[@*[contains(translate(name(.),'n','N'),'Name')]]")
+				$namedNodes | ForEach-Object {
+
+					$wpfObjects.Add($_.Name, $tempform.FindName($_.Name))
+
+				} #foreach-object
+			} #foreach xamlpath
+		} #try
+		catch
+		{
+			throw $error[0]
+		} #catch
+	} #PROCESS
+
+	END
+	{
+		Write-Output $wpfObjects
+	} #END
+}
+
+
+#region Blah
+$path = "$scriptDirectory"
+
+$wpf = Get-ChildItem -Path $path -Filter *.xaml -file | Where-Object { $_.Name -ne 'App.xaml' } | Get-XamlObject
+$wpf
